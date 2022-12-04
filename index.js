73cde6bead1af6da6bb9750287af07f8a97c64d9
KiaFIfe <likiafife1@googlemail.com>
2022-12-04 13:38:45 +0000

    Added the Js to make the calculator functional

---
+++ index.js
@@ -0,0 +1,21 @@
+let result = document.querySelector("#display");
+
+let calculate = (number) => {
+  result.value += number;
+};
+
+let equal = () => {
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    alert("please enter a valid response");
+  }
+};
+
+function clr() {
+  result.value = "";
+}
+
+function del(){
+  result.value = result.value.slice(0,-1)
+}
